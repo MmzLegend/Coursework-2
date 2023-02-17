ba87a6515656de413b06a64268e4a602f3d30936
Muhammad Mukhtar Zikirullahi <mukhtarzik@gmail.com>
2023-02-17 06:54:12 +0400

    server commit

--- server.js
+++ server.js
@@ -22,6 +22,83 @@ app.use((req,res,next)=>{
     next();
 })
 
+//mongo db connection
+let db;
+
+MongoClient.connect("mongodb+srv://Mmzlegend23:Mxkht2721@cluster0.k3258d9.mongodb.net", (error, client) => {
+    db = client.db("Coursework2");
+});
+
+app.use(express.static("./"));
+
+
+app.get('/',(req,res,next)=>{
+    res.send('Please select a collection e.g /collection/messages')
+})
+
+
+app.param("collectionName", (req, res, next, collectionName)=>{
+    req.collection = db.collection(collectionName);
+    return next();
+});
+
+
+app.get("/collection/:collectionName", (req, res, next)=>{
+    req.collection.find({}).toArray((e, results) => {
+        if (e) return next();
+        res.send(results);
+    });
+});
+
+
+//adding a post to save new order
+app.post('/collection/:collectionName' , (req, res, next)=>{
+    req.collection.insertMany(req.body, (e, results) => {
+        if (e) return next(e);
+        res.send(results.ops);
+    });
+});
+
+// updating a collection object
+app.put('/collection/:collectionName/:id',(req,res,next)=>{
+    req.collection.update(
+        {_id:new objectId(req.params.id)},
+        {$set:req.body},
+        {safe:true, multi:false},
+        (e,result)=>{
+            if(e)return next(e)
+            res.send(result.result.n ===1)?{'message':'Success'}:{'message':'Error'}
+        }
+    )
+})
+
+// search
+app.get('/collection/:collectionName/search', (req, res, next) => {
+    let query_str = req.query.key_word
+    req.collection.find({}).toArray((e, results) => {
+        if (e) return next(e)
+        let newList = results.filter((lesson) => {
+            return lesson.subject.toLowerCase().match(query_str) || lesson.location.toLowerCase().match(query_str)
+        });
+        res.send(newList)
+    })
+})
+
+
+// serving static files
+   app.use((req,res,next)=>{
+    var filePath = path.join(__dirname, 'images',req.url)
+    fs.stat(filePath, function(err,fileInfo){
+        if(err){
+            next();
+            return ;
+        }
+        if(fileInfo.isFile) res.send(filePath)
+        else next()
+    })
+})
+
+
 
 //error handler
 app.use(function(req,res){
